a24b28d706ddeda37bf76b5fc405d4c5f2984ff4
JeonDaehong <daehong770@gmail.com>
2025-02-02 22:34:32 +0900

    data image update

--- lol-champion-event-generator/src/main/java/lol/event/Main.java
+++ lol-champion-event-generator/src/main/java/lol/event/Main.java
@@ -1,6 +1,7 @@
 package lol.event;
 
 import lol.event.util.SessionUtil;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.OffsetDateTime;
@@ -20,6 +21,7 @@ public class Main {
 
     // Variable Common Code
     public static Integer GAME_COUNT = 0;
+    public static Integer THREAD_COUNT = 0;
     public static String BOOT_SERVER = null;
 
     public static void main(String[] args) {
@@ -27,7 +29,7 @@ public class Main {
 
         // args[0] = 게임 수
         // args[1] = 부트스트랩 서버:포트
-        if (args.length < 2) {
+        if (args.length < 3) {
             log.info("올바르지 않은 사용으로 인하여, 프로그램을 종료합니다.");
             log.info("사용법: java Main <게임 수> <bootstrap 서버:포트> 입니다.");
             System.exit(0);
@@ -38,10 +40,17 @@ public class Main {
             log.info("최소 한 게임 이상은 입력해주시기 바랍니다.");
             System.exit(0);
         }
+        
+        if ( Integer.parseInt(args[1]) < 1) {
+            log.info("올바르지 않은 사용으로 인하여, 프로그램을 종료합니다.");
+            log.info("사용 할 스레드를 최소 1개 이상 입력해주시기 바랍니다.");
+            System.exit(0);
+        }
 
         // Common Code Setting
         GAME_COUNT = Integer.parseInt(args[0]);
-        BOOT_SERVER = args[1];
+        THREAD_COUNT = Integer.parseInt(args[1]);
+        BOOT_SERVER = args[2];
 
         // Game Data Collection
         collectGameData();
@@ -51,12 +60,20 @@ public class Main {
     public static void collectGameData() {
 
         CountDownLatch latch = new CountDownLatch(GAME_COUNT);
-        ExecutorService executor = Executors.newFixedThreadPool(GAME_COUNT);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
 
         IntStream.range(0, GAME_COUNT).forEach(j -> {
-            String sessionGameID = SessionUtil.getSessionGameID();
-            OffsetDateTime createGameDate = OffsetDateTime.now(ZoneId.of("UTC"));
             executor.execute(() -> {
+                // 200 ~ 1000ms 사이의 랜덤 딜레이 적용
+                long delay = 200 + (long) (Math.random() * 800);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(delay);
+                } catch (InterruptedException e) {
+                    log.error("[ ERROR ] Thread Sleep Interrupted Error: {}", e.getMessage());
+                    throw new RuntimeException(e);
+                }
+                String sessionGameID = SessionUtil.getSessionGameID();
+                OffsetDateTime createGameDate = OffsetDateTime.now(ZoneId.of("UTC"));
                 new Game(sessionGameID, createGameDate, latch).run();
             });
         });
